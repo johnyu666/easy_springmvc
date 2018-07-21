6c05be5d08dba0ec09c77ecef43e76f96ccd5bd2
yuyinghan@yeah.net <yuyinghan@yeah.net>
2018-07-21 13:03:49 +0800

    aa

---
+++ src/main/java/pojo/User.java
@@ -0,0 +1,44 @@
+package pojo;
+
+public class User {
+	private Integer id;
+	private String uname;
+	private boolean sex;
+	private int age;
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUname() {
+		return uname;
+	}
+
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+
+	public boolean isSex() {
+		return sex;
+	}
+
+	public void setSex(boolean sex) {
+		this.sex = sex;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public static void main(String[] args) {
+		
+	}
+}
